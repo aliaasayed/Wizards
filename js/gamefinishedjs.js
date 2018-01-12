da03bf65e35f4e43fb502a1b3a1a5352ebcfa91a
rania-alaa <rania.alaaeldien.mohamed@gmail.com>
2018-01-12 18:41:39 +0200

    update gamefinished

---
+++ js/gamefinishedjs.js
@@ -0,0 +1,26 @@
+var sndflag=0;
+var sound=document.getElementById("sound");
+sound.addEventListener("click", soundFunction);
+
+var clicksnd = new Audio("music/Click2.mp3");
+var snd = new Audio("music/gamefinished.mp3");
+
+window.onload = function() {
+    snd.play();
+}
+snd.loop=true;
+function soundFunction(){
+	clicksnd.play();
+	if(sndflag===0)
+	{
+		snd.pause();
+	    sound.innerHTML='<img src="img/nosound.png" />';
+	    sndflag=1;
+	}
+	else if (sndflag===1)
+	{
+		snd.play();
+	    sound.innerHTML='<img src="img/sound.png" />';
+	    sndflag=0;
+	}
+}
